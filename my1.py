9e672a6ab8cf037b951dd01a85523731f31258ed
Yuriy Pervushin <vp.russian@yandex.ru>
2021-11-07 20:56:16 +0500

    added files

--- my1.py
+++ my1.py
@@ -1,14 +1,33 @@
-import os
-list_ = []
-list_name = os.listdir('sorted')
-for i in list_name:
-    with open(f'sorted/{i}', encoding='utf-8') as file:
-        lines = file.readlines()
-        lines = [i + '\n'] + [str(len(lines)) + '\n'] + lines
-        list_.append(lines)
-print(list_)
-text = ' '.join(sum((sorted(list_, key=len)), []))
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+KEYWORDS = ['дизайн', 'фото', 'web', 'python']
+
+resp = requests.get('https://habr.com/ru/all/')
+soup = BeautifulSoup(resp.text, 'html.parser')
 
 
-with open('end.txt', 'w', encoding='utf-8') as end:
-    end.write(text)
+articles = soup.find_all('article')
+
+for article in articles:
+    title_element = article.find('a', class_='post__title_link')
+    title_text = title_element.text.strip()
+
+    hubs = [hub.text.strip().lower() for hub in article.find_all('a', class_='hub-link')]
+    preview = article.find('div', class_='post__text-html').text.strip().lower()
+
+    href = title_element.attrs.get('href')
+    res = requests.get(href)
+    soup_ = BeautifulSoup(res.text, 'html.parser')
+
+    body_element = soup_.find('div', class_='post__text')
+    body = body_element.text.strip().lower()
+
+    for desired in KEYWORDS:
+        if desired in title_text.lower() or desired in hubs or desired in preview or desired in body:
+            data_element = soup_.find('span', class_='post__time')
+            data_text = data_element.attrs.get('data-time_published')
+            date_old = datetime.strptime(data_text, "%Y-%m-%dT%H:%MZ")
+            data = datetime.strftime(date_old, "%d-%m-%Y")
+            print(data, title_text, href)
